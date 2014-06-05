e4902594aad293f22317d598891f9b3a52ef191b
Hack Reactor Students <wlabranche@gmail.com>
2014-06-05 16:29:57 -0700

    Completed Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    //var  val = {value: value, front: value, back: null}
+    var node = makeNode(value);
+
+    if (!list.head){
+      list.head = node;
+      list.tail = list.head;
+    }else{
+      //else add add new tail & make that head too
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function(){
+    var headResult = list.head.value;
+    list.head = list.head.next;
+    return headResult;
   };
 
   list.contains = function(target){
+
+    var search = function(node){
+      var result = false;
+
+      if (node.value === target){
+        result = true;
+      }else if(node.next === null){
+        return result;
+      }else{
+        return search(node.next);
+      }
+
+      return result;
+    };
+
+    return search(list.head);
   };
 
   return list;
