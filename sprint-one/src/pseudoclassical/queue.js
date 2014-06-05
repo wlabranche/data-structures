15b95eabd6da9448130b59796156fd55aa1afe3a
Hack Reactor Students <wlabranche@gmail.com>
2014-06-05 10:26:31 -0700

    Built pseudoclassical instantiations

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._size = 0;
+  this._storage = {};
+};
+Queue.prototype.size = function(){
+  return this._size;
+};
+Queue.prototype.enqueue = function(value){
+  this._storage[this._size] = value;
+  this._size++;
+};
+Queue.prototype.dequeue = function(){
+  var result = 0;
+  if (this._size){
+    this._size--;
+    result = this._storage[0];
+    for (var key in this._storage){
+      this._storage[key] = this._storage[+key + 1];
+    }
+    delete this._storage[this._size];
+  }
+  return result;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._size = 0;
+  this._storage = {};
 };
 
+Stack.prototype.size = function(){
+  return this._size;
+};
+
+Stack.prototype.push = function(value){
+  this._storage[this._size] = value;
+  this._size++;
+};
+Stack.prototype.pop = function(){
+  var result = 0;
+  if (this._size){
+    this._size--;
+    result = this._storage[this._size];
+    delete this._storage[this._size];
+  }
+  return result;
+}
 
